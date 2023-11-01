9e3b73d326ea0e214ea7b9c9c9ea36debeb9f701
yuzhaojun <yuzhaojun@kuaishou.com>
2023-11-02 03:52:27 +0800

    feat: 0.0.2

---
+++ mytime.js
@@ -0,0 +1,2 @@
+const now = new Date().toLocaleString();
+console.log(`云舟提醒您，现在是： ${now}`);
--- mytime.rb
+++ mytime.rb
@@ -1,8 +1,8 @@
 class Mytime < Formula
     homepage "https://github.com/SmallFishCode/homebrew-mytime"
-    url "https://github.com/SmallFishCode/homebrew-mytime/releases/download/v0.0.1/mytime-v0.0.1.tar.gz"
+    url "https://github.com/SmallFishCode/homebrew-mytime/releases/download/v0.0.2/mytime-v0.0.2.tar.gz"
     sha256 "f173a64b2e2317afc77095970b4a8964eeab87c38d9c35331da17af2c59e5706"
-    version "0.0.1"
+    version "0.0.2"
   
   
     def install
