d91b5b66f693e6eac070d194cfc0fc2986c7f3b6
yuzhaojun <yuzhaojun@kuaishou.com>
2023-11-02 04:03:07 +0800

    feat: 更改rb

--- mytime.rb
+++ mytime.rb
@@ -5,11 +5,23 @@ class Mytime < Formula
     version "0.0.2"
   
   
+    depends_on "node"
+
     def install
-        bin.install "mytime.js"
+        # 1. 创建一个新目录来存放要安装的文件
+        prefix.install Dir['*']
+
+        # 2. 创建一个带有 shebang 的文件，并包含在 mytime.js 中的内容
+        File.write("mytime", "#!/usr/bin/env node\n" + File.read("#{prefix}/mytime.js"))
+
+        # 3. 为新创建的 'mytime' 文件添加可执行权限
+        chmod 0755, "mytime"
+
+        # 4. 将可执行文件移动到 bin 目录
+        bin.install "mytime"
     end
-  
+
     test do
-        system "false"
+        assert_equal "现在是", shell_output("#{bin}/mytime").strip[0,3]
     end
   end
