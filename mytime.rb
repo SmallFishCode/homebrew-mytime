6b880c1b997cb9940b11836c6c8851cde201e9f5
yuzhaojun <yuzhaojun@kuaishou.com>
2023-11-02 03:18:30 +0800

    feat: 更新安装命令

--- mytime.rb
+++ mytime.rb
@@ -4,14 +4,12 @@ class Mytime < Formula
     sha256 "f173a64b2e2317afc77095970b4a8964eeab87c38d9c35331da17af2c59e5706"
     version "0.0.1"
   
-    depends_on "node"
   
     def install
-        system "npm", "install", *std_npm_install_args(libexec)
-        bin.install_symlink Dir["#{libexec}/bin/*"]
-      end
+        bin.install "mytime"
+    end
   
     test do
-      assert_equal "现在是", shell_output("#{bin}/mytime").strip[0,3]
+        system "false"
     end
   end
